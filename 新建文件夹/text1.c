9d71acd711687b10f99ff4fbd45a6f5cb2a07ed5
unknown <l>
2019-07-15 23:15:25 +0800

    新建文件夹

---
+++ 新建文件夹/text1.c
@@ -0,0 +1,277 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+//int  jiecheng(int x)
+//{
+//	if (x > 0)
+//		return jiecheng(x - 1)*x;
+//	else
+//		return 1;
+//}
+//int main()
+//{/*
+//	int a;
+//	char *array =  "abcd" ;
+//	char *p = array;
+//	char *p1 = array;
+//	printf("请输入你要左旋几个字符\n");
+//	scanf("%d", &a);
+//	p = p + a;
+//	char *p3 = p;
+//	printf("%s", p);
+//	while (p1 < p3)
+//	{
+//		printf("%c", *p1);
+//		p1++;
+//	}
+//		*/
+//
+//	//char *array = "abcdef";
+//	//char *p;
+//	//p = array;
+//	//p = p + 1;
+//	//printf("%c", *p);
+//	/*int r, m, n, p;
+//	printf("请输入你要判断的两个数字\n");
+//	scanf("%d%d", &m, &n);
+//	if (m > n)
+//	{
+//		int temp = n;
+//		n = m;
+//		m = temp;
+//	}
+//	p = m*n;
+//	while (m != 0)
+//	{
+//		r = n%m;
+//		n = m;
+//		m = r;
+//	}
+//	printf("最大公约数%d\n", n);
+//	printf("最小公倍数%d", p / n);*/
+//
+//
+//	//char *p = "abcdef";
+//	//printf("%s", p);
+//	/*char c = getchar();
+//	printf("%s", c);*/
+//	/*char a = 'a';
+//	printf("%c", a);*/
+//
+//	/*int a;
+//	int num;
+//	int sum = 0;
+//	int count = 0;
+//	printf("请输入你所要编辑的数字\n");
+//	scanf("%d", &a);
+//	num = a;
+//	while (count < 4)
+//	{
+//		num = num*10 + a;
+//		sum = sum + num;
+//		count++;
+//	}
+//	printf("%d", sum + a);*/
+//
+//	/*int i;
+//	int j;
+//	int a;
+//	int num ;
+//	int sum = 0;
+//	printf("请输入你要求的阶乘上限\n");
+//	scanf("%d", &a);
+//	for (i = 1; i <= a; i++)
+//	{
+//		num = 1;
+//		for (j = 1; j <= i; ++j)
+//		{
+//			num = num*j;
+//		}
+//		sum = sum + num;
+//	}*/
+//	/*int a;
+//	int i;
+//	int c;
+//	int sum = 0;
+//	scanf("%d", &a);
+//	for (i = 1; i <= a; ++i)
+//	{
+//		for (int j = 1; j <= i; ++j)
+//		{
+//			c = jiecheng(j);
+//		}
+//		sum = sum + c;
+//	}
+//	printf("%d", sum);*/
+//
+//
+//	system("pause");
+//	return 0;
+//
+int main()
+{
+	int array[10];
+	for (int i=0; i < 10; i++)
+	{
+		scanf("%d", array[i]);
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
